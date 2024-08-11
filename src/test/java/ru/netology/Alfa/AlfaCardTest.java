c0809fb64d7af90a502ad6dc5e9b6cecf388217b
SergGrebnev <serggrebnev79@gmail.com>
2024-08-12 00:18:39 +0700

    version 1 from the webinar

--- src/test/java/ru/netology/Alfa/AlfaCardTest.java
+++ src/test/java/ru/netology/Alfa/AlfaCardTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class AlfaCardTest {
@@ -39,9 +42,17 @@ public class AlfaCardTest {
     }
 
     @Test
-    public void testFormCard()
-    {
+    public void testFormCard() throws InterruptedException {
         driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Сергей");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79130000000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim()); //.trim() - удаляет лишние пробелы в начале и в конце строки
+        //Thread.sleep(10000);
     }
 
 
